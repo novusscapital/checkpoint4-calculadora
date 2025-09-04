d1b3b7b7a582274bcb750b08bf7c4d643fc65186
matheus <matheusbispo0404@gmail.com>
2025-09-04 15:19:07 -0300

    header structure

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        // fragments
+        <>
+            <div className="header">
+                <h1>Calculadora Virtual</h1>
+
+            </div>
+        </>
+    )
+}
+export default Header
